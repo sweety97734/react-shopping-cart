a3f4a4817067c87c6c922a436f3e9267b480cbb2
sweety97734 <sweetyprasad97@gmail.com>
2020-10-05 13:17:36 +0530

    animation modal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
 
 export default class Products extends Component {
     render() {
         return (
             <div>
+                <Fade bottom cascade>
                 <ul className="products">
                     {this.props.products.map(product => (
                         <li key={product._id}>
                             <div className="product">
-                                <a href={"#" + product._id}>
+                                <a href={"#" + product._id} >                                
                                   <img src={product.image} alt={product.title}></img>
                                   <p>{product.title}</p>
                                 </a>
@@ -22,6 +24,7 @@ export default class Products extends Component {
                         </li>
                     ))}
                 </ul>
+              </Fade>
             </div>
         );
     }
